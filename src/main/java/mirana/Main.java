660dfea91b1de9301728c6c4d5c47f5a00d6bf49
唐雁 <tangyan1412@foxmail.com>
2017-05-16 21:08:56 +0800

    add gradle to run

--- src/main/java/mirana/Main.java
+++ src/main/java/mirana/Main.java
@@ -1,27 +1,35 @@
 package mirana;
 
-import mirana.util.base.FileUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import mirana.util.base.Global;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        FileUtil.deleteFiles(new File(Config.mdOutputPath));
-
-        for (String path : Config.entityPath) {
-            executeJavaDoc(EntityHandle.class, path);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String config = "";
+        for (String arg : args) {
+            config += arg;
         }
+        Map<String, Object> configs = objectMapper.readValue(config, Map.class);
 
-        for (String path : Config.enumerationPath) {
-            executeJavaDoc(EnumerationHandle.class, path);
-        }
+//        FileUtil.deleteFiles(new File(Config.mdOutputPath));
+//
+//        for (String path : Config.entityPath) {
+//            executeJavaDoc(EntityHandle.class, path);
+//        }
+//
+//        for (String path : Config.enumerationPath) {
+//            executeJavaDoc(EnumerationHandle.class, path);
+//        }
+//
+//        for (String path : Config.interfacePath) {
+//            executeJavaDoc(InterfaceHandle.class, path);
+//        }
 
-        for (String path : Config.interfacePath) {
-            executeJavaDoc(InterfaceHandle.class, path);
-        }
     }
 
     private static void executeJavaDoc(Class handle, String javaFilePath) {
